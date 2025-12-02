c53b87053ae089e16876a6a76610c5d519e32fcf
enzo-prism <215033440+enzo-prism@users.noreply.github.com>
2025-12-02 08:20:33 -0800

    Remove extra dining gallery image

--- app/dining/page.tsx
+++ app/dining/page.tsx
@@ -29,7 +29,6 @@ export default function Page() {
               IMAGES.waffles,
               IMAGES.salad,
               IMAGES.drinksBar,
-              IMAGES.wedding,
               IMAGES.romanticViews,
             ]}
           />
